8e8909e8d8274e8e72be9af4cd2d7bc83a31dc93
marinabart <marinabartzokis@gmail.com>
2017-07-26 19:04:40 -0400

    Adding updated files to repo

---
+++ freestyle.py
@@ -0,0 +1,48 @@
+# OBJECTIVE
+    # Retrieve statistics about a given Twitter user, such as that user's number of followers.
+    # Optionally store these metrics in a CSV file to track changes over time.
+
+# INPUTS
+    # A list of existing tweets and/or hashtags and/or usernames on Twitter.com.
+
+# OUTPUTS
+    # A CSV file or database inventory of Twitter tweets and/or hashtags and/or usernames.
+
+# Set a new environment variable called  NYU_INFO_2335
+echo export NYU_INFO_2335="SecretPassword123" >> ~/.bash_profile
+
+
+# Need to add an 'Environment Variable' to hide API Key from public on GitHub
+# File Operations
+import os
+os.listdir("/Users/marinabartzokis/Desktop/freestyle")
+
+# Directory Operations
+import os
+os.getcwd()
+os.chdir("/Users/marinabartzokis/Desktop")
+os.mkdir("/Users/marinabartzokis/Desktop/freestyle")
+
+#Accessing Environment Variables
+import os
+os.environ["NYU_INFO_2335"] #> SecretPassword123
+
+
+
+
+
+
+#TWITTER_API_KEY #IRWofH2ogy4dwf8cPxrMpiXw0
+#TWITTER_API_SECRET #l9TwXT1pupc5QuTWLz0U7n2hfntUE4yO0x2nMO0S0X2MOSKnTS
+#TWITTER_ACCESS_TOKEN #848698383967678464-WE45shQKyiO4CKfpiMoNoShQgq5NAMg
+#TWITTER_ACCESS_TOKEN_SECRET #yDwj9vplgBNwE1gnj2urQ2imRhwbCt6gZxUzuHNwWU2DT
+
+
+
+
+
+
+
+
+# For Homebrew-installed Python 3.x on Mac OS:
+pip install tweepy
